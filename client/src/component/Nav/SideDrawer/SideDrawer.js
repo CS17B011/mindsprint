56ab59fc0fd15ebd5f1c98b72239fad534477ea9
krish7777 <krishnendusudheeh4@gmail.com>
2019-07-04 20:56:02 +0530

    added animation to side-nav

--- client/src/component/Nav/SideDrawer/SideDrawer.js
+++ client/src/component/Nav/SideDrawer/SideDrawer.js
@@ -1,35 +1,44 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "./SideDrawer.css";
+import './SideDrawer.css';
 
 const sideDrawer = props => {
-  let drawerClasses = "side-drawer";
+  let drawerClasses = 'side-drawer';
   if (props.show) {
-    drawerClasses = "side-drawer open";
+    drawerClasses = 'side-drawer open';
   }
+  const navLinks = document.querySelectorAll('li');
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `linkFade 0.5s ease forwards ${index / 7 - 0.9}s`;
+    }
+  });
+
   return (
     <nav className={drawerClasses}>
-      <ul>
+      <ul className='link-tab'>
         <li>
-          <Link to = "/">HOME</Link>
+          <Link to='/'>HOME</Link>
         </li>
         <li>
-          <Link to = "/">ABOUT</Link>
+          <Link to='/'>ABOUT</Link>
         </li>
         <li>
-          <Link to = "/">DOWNLOADS</Link>
+          <Link to='/'>DOWNLOADS</Link>
         </li>
         <li>
-          <Link to = "/">FAQ</Link>
+          <Link to='/'>FAQ</Link>
         </li>
         <li>
-          <Link to = "/">CONTACT US</Link>
+          <Link to='/'>CONTACT US</Link>
         </li>
         <li>
-          <Link to = "/login">LOGIN</Link>
+          <Link to='/login'>LOGIN</Link>
         </li>
         <li>
-          <Link className="special-button__mobile" id="onReg" to = "/signin">
+          <Link className='special-button__mobile' id='onReg' to='/signin'>
             REGISTER NOW
           </Link>
         </li>
