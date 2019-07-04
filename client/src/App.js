c8cc01df0bad12e3bfe7532a1754641693b88b5a
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2019-07-04 17:52:24 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Fragment } from "react";
 import "./App.css";
 import Navbar from "./component/Nav/Navbar";
 import Corousel from "./component/corousel/Corousel";
 import Footer from "./component/footer/footer";
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Register from './component/auth/register';
+import Login from './component/auth/login';
 import Jumbo from "./component/jumbotron/jumbotron";
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route to="/" component={Corousel} />
-
-          <Route to="/" component={Jumbo} />
-          <Switch />
-
-          <Footer />
-        </Fragment>
-      </Router>
+class App extends React.Component {
+  render(){
+      return ( 
+        <BrowserRouter>
+          <Fragment>
+            <div>
+              <Navbar/>
+            </div>
+            <div>
+              <Route path = "/" exact component = {Corousel}></Route>
+              <Route path = "/signin" exact  component = {Register}></Route>
+              <Route path = "/login" exact  component = {Login}></Route>
+              <Route path="/" exact component={Jumbo} />
+            </div>
+            <div>
+              <Footer/>
+            </div>
+          </Fragment>
+        </BrowserRouter>
     );
   }
 }
