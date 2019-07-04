8b0f9482be7b6352f87eed4b5cd2fa200e1919b0
krish7777 <krishnendusudheeh4@gmail.com>
2019-07-04 21:16:42 +0530

    removed the error of all li moving

--- client/src/component/Nav/SideDrawer/SideDrawer.js
+++ client/src/component/Nav/SideDrawer/SideDrawer.js
@@ -7,37 +7,38 @@ const sideDrawer = props => {
   if (props.show) {
     drawerClasses = 'side-drawer open';
   }
-  const navLinks = document.querySelectorAll('li');
+  const navLinks = document.querySelectorAll('li.move');
+  console.log(navLinks);
   navLinks.forEach((link, index) => {
     if (link.style.animation) {
       link.style.animation = '';
     } else {
-      link.style.animation = `linkFade 0.5s ease forwards ${index / 7 - 0.9}s`;
+      link.style.animation = `linkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
     }
   });
 
   return (
     <nav className={drawerClasses}>
       <ul className='link-tab'>
-        <li>
+        <li className='move'>
           <Link to='/'>HOME</Link>
         </li>
-        <li>
+        <li className='move'>
           <Link to='/'>ABOUT</Link>
         </li>
-        <li>
+        <li className='move'>
           <Link to='/'>DOWNLOADS</Link>
         </li>
-        <li>
+        <li className='move'>
           <Link to='/'>FAQ</Link>
         </li>
-        <li>
+        <li className='move'>
           <Link to='/'>CONTACT US</Link>
         </li>
-        <li>
+        <li className='move'>
           <Link to='/login'>LOGIN</Link>
         </li>
-        <li>
+        <li className='move'>
           <Link className='special-button__mobile' id='onReg' to='/signin'>
             REGISTER NOW
           </Link>
