df52fdcc2918ed1f56fff02aa97fc87e2d0afc27
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2019-07-04 10:38:50 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import Navbar from "./component/Nav/Navbar";
 import Corousel from "./component/corousel/Corousel";
 import Footer from "./component/footer/footer";
-
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Navbar />
-        <Corousel />
-        <Footer />
-      </Fragment>
+import {BrowserRouter,Route} from 'react-router-dom';
+import Login from './component/auth/login';
+import Register from './component/auth/register';
+class App extends React.Component {
+  render(){
+      return ( 
+        <BrowserRouter>
+          <Fragment>
+            <div>
+              <Navbar/>
+            </div>
+            <div style = {{display:"flex",justifyContent:"center"}}>
+              <Route path = "/" exact component = {Corousel}></Route>
+              <Route path = "/login" exact component = {Login}></Route>
+              <Route path = "/signin" exact component = {Register}></Route>
+            </div>
+            <div>
+              <Footer/>
+            </div>
+          </Fragment>
+        </BrowserRouter>
     );
   }
 }
