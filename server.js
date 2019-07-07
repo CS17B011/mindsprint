3dbf32675ee92ec341a41f39036827689c004162
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2019-07-07 18:20:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-
+const passport=require('passport');
 app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(passport.initialize());
+app.use(cors());
 //Database URL
 var dbUrl = require('./config/env').dbUrl;
 
@@ -16,6 +20,8 @@ mongoose.connect(dbUrl, {useNewUrlParser: true, useCreateIndex: true})
 //API Routes
 app.use('/api/questions', require('./routes/api/questions'));
 app.use('/api/faqs', require('./routes/api/faqs'));
+app.use('/api/students',require('./routes/api/students'));
+app.use('/api/organizers',require('./routes/api/organizers'));
 
 const port = process.env.PORT || 5000;
 
