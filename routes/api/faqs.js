80571f369375bc60a77f2fbab3800c8b2767b44d
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2019-07-07 18:11:49 +0530

    Update faqs.js

--- routes/api/faqs.js
+++ routes/api/faqs.js
@@ -20,7 +20,10 @@ router.post('/', (req,res) => {
     question: req.body.question,
     answer: req.body.answer
   });
-  newFAQ.save().then(ques => res.json(ques));
+  newFAQ.save().then(ques => res.json(ques))
+                .catch((err)=>{
+                  res.json({"error":"something went wrong"})
+                });
 });
 
 
@@ -31,7 +34,7 @@ router.put('/:id', (req,res) => {
   FAQ.updateOne({"_id": req.params.id},
       {$set: {"answer": req.body.answer}})
       .then(answer => res.json(answer))
-      .catch(err => res.status(400).json({"msg" : "Bad Request!!"}));
+      .catch(err => res.status(400).json({"error" : "Bad Request!!"}));
 });
 
 
