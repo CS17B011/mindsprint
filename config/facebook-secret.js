3f6e4a9faa5a054284f7435e299a9fb88eaab2ca
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2019-07-08 19:45:24 +0530

    Add files via upload

---
+++ config/facebook-secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    clientID:"346810166006111",
+    clientSecret:"18ebaaba323cd4ca47540c0aade7c46a"
+}
---
+++ config/google-secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    clientID:"648069858587-5juhlr9q9b0r4qm5a7pe7f60ogbdnjv8.apps.googleusercontent.com",
+    clientSecret:"LTAMx2RuHGFYeICfOqEbtQuw"
+}
